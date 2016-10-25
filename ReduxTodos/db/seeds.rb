c45733d3eecf8e0862a9ba3c3ff0f11e47ae5405
App Academy Student <student@appacademy.io>
2016-10-25 13:44:29 -0700

    first commit

---
+++ ReduxTodos/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Todo.create!(title: "Smash the T.V.", body: "The news anchors keep yelling at me, and it makes me sad", done: "true")
+Todo.create!(title: "Fail next assessment", body: "To lower the average passing score", done: "true")
+Todo.create!(title: "Sleep with the gardner", body: "I need to cheat on my husband with the gardner, and possibly the pool boy too.", done: "true")
